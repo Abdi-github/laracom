359bf3f78927a72ce9475b0df036955a3402aa54
Abdulkadir Ahmed Hussien <55865135+Abdi-github@users.noreply.github.com>
2023-08-11 03:16:35 +0200

    modifiy vite config

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,12 +3,12 @@ import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     server: {
-        host: "0.0.0.0",
-        hmr: {
-            // clientPort: 5173,
-            host: "laracom.swiftapp.tech",
-            protocol: "ws",
-        },
+        // host: "0.0.0.0",
+        // hmr: {
+        //     // clientPort: 5173,
+        //     host: "localhost",
+        //     protocol: "ws",
+        // },
         port: 8020,
     },
     plugins: [
